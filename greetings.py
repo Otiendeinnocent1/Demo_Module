9cdfeacec9106438609dad6f845689da45eb31aa
Otiendeinnocent1 <otiendeinnocent@gmail.com>
2025-05-12 20:46:02 +0300

    First commit

---
+++ greetings.py
@@ -0,0 +1,15 @@
+#print("I am innocent.This is my first code")
+#this was my first code
+#print(name)
+"""
+name = 'Ganase Innocent Otiende'
+print(name) # this statements prints the value stored in variable name
+name = 'artist'
+print(name)
+"""
+my_car = ["orange
+          ", 78, "marks"]
+#print(my_car)clear
+
+#print(type(my_car))
+print(my_car[1])
